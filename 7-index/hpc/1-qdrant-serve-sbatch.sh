1187cf929f74eb28cea55e81fdb13879cc88782e
Kieran Mc Gowan <kieranmcgowan21@hotmail.com>
2026-09-26 00:08:56 +0100

    fix quadrant bind

--- 7-index/hpc/1-qdrant-serve-sbatch.sh
+++ 7-index/hpc/1-qdrant-serve-sbatch.sh
@@ -68,6 +68,7 @@ singularity exec \
     --rocm \
     --bind "$QDRANT_STORAGE_DIR:/qdrant/storage" \
     --env QDRANT__SERVICE__HTTP_PORT=${QDRANT_PORT} \
+    --env QDRANT__STORAGE__STORAGE_PATH=/qdrant/storage \
     "$QDRANT_SIF" \
     bash -c "
         export LD_LIBRARY_PATH=/opt/rocm/lib:/opt/rocm/lib64:/usr/local/lib
